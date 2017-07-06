4d36b8a9c0d31c19c0d5e1106383a82bd02c29d6
greyby <greyby.5@gmail.com>
2017-07-06 20:13:03 +0800

    add application launch

--- modules/system.lua
+++ modules/system.lua
@@ -1,6 +1,17 @@
 local hotkey = require "hs.hotkey"
-local lock_screen_key = "`"
+local lock_screen_key = "L"
 
 hotkey.bind(hyper, lock_screen_key, function()
-  os.execute("/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend")
+  hs.caffeinate.lockScreen()
+  -- hs.caffeinate.startScreensaver()
 end)
+
+-- mute on sleep
+function muteOnWake(eventType)
+  if (eventType == hs.caffeinate.watcher.systemDidWake) then
+    local output = hs.audiodevice.defaultOutputDevice()
+    output:setMuted(true)
+  end
+end
+caffeinateWatcher = hs.caffeinate.watcher.new(muteOnWake)
+caffeinateWatcher:start()
